a5d9abd595d82b7311c5a44d6c0cb5ee086ba452
heliopn <hpaivan@gmail.com>
2020-09-22 21:46:32 -0300

    1.3

--- T2/api/routers/task.py
+++ T2/api/routers/task.py
@@ -1,94 +1,50 @@
-# pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring
 import uuid
-
 from typing import Optional, Dict
-
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import APIRouter, FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
+from ..database import DBSession, get_db
+from ..models import Task
 
+router = APIRouter()
 
-# pylint: disable=too-few-public-methods
-class Task(BaseModel):
-    description: Optional[str] = Field(
-        'no description',
-        title='Task description',
-        max_length=1024,
-    )
-    completed: Optional[bool] = Field(
-        False,
-        title='Shows whether the task was completed',
-    )
-
-    class Config:
-        schema_extra = {
-            'example': {
-                'description': 'Buy baby diapers',
-                'completed': False,
-            }
-        }
-
-
-tags_metadata = [
-    {
-        'name': 'task',
-        'description': 'Operations related to tasks.',
-    },
-]
-
-app = FastAPI(
-    title='Task list',
-    description='Task-list project for the **Megadados** course',
-    openapi_tags=tags_metadata,
-)
-
-class DBSession:
-    tasks={}
-    def __init__(self):
-        self.tasks = DBSession.tasks
-
-def get_db():
-    return DBSession()
-
-
-@app.get(
-    '/task',
-    tags=['task'],
+@router.get(
+    '/',
+    # tags=['task'],
     summary='Reads task list',
     description='Reads the whole task list.',
     response_model=Dict[uuid.UUID, Task],
 )
 async def read_tasks(completed: bool = None, db: DBSession = Depends(get_db)):
-    if completed is None:
-        return db.tasks
-    return {
-        uuid_: item
-        for uuid_, item in db.tasks.items() if item.completed == completed
-    }
+    try:
+        return db.read_tasks(completed)
+    except KeyError as exception:
+        raise HTTPException(
+            status_code=404,
+            detail='Task not found',
+        ) from exception
 
 
-@app.post(
-    '/task',
-    tags=['task'],
+@router.post(
+    '/create',
+    # tags=['task'],
     summary='Creates a new task',
     description='Creates a new task and returns its UUID.',
     response_model=uuid.UUID,
 )
 async def create_task(item: Task, db: DBSession = Depends(get_db)):
-    uuid_ = uuid.uuid4()
-    db.tasks[uuid_] = item
-    return uuid_
+    return db.create_task(item)
 
 
-@app.get(
-    '/task/{uuid_}',
-    tags=['task'],
+@router.get(
+    '/{uuid_}',
+    # tags=['task'],
     summary='Reads task',
     description='Reads task from UUID.',
     response_model=Task,
 )
 async def read_task(uuid_: uuid.UUID, db: DBSession = Depends(get_db)):
     try:
-        return db.tasks[uuid_]
+        return db.read_task(uuid_)
     except KeyError as exception:
         raise HTTPException(
             status_code=404,
@@ -96,15 +52,15 @@ async def read_task(uuid_: uuid.UUID, db: DBSession = Depends(get_db)):
         ) from exception
 
 
-@app.put(
-    '/task/{uuid_}',
-    tags=['task'],
+@router.put(
+    '/replace/{uuid_}',
+    # tags=['task'],
     summary='Replaces a task',
     description='Replaces a task identified by its UUID.',
 )
 async def replace_task(uuid_: uuid.UUID, item: Task, db: DBSession = Depends(get_db)):
     try:
-        db.tasks[uuid_] = item
+        db.replace_task(uuid_,item)
     except KeyError as exception:
         raise HTTPException(
             status_code=404,
@@ -112,16 +68,15 @@ async def replace_task(uuid_: uuid.UUID, item: Task, db: DBSession = Depends(get
         ) from exception
 
 
-@app.patch(
-    '/task/{uuid_}',
-    tags=['task'],
+@router.patch(
+    '/alter/{uuid_}',
+    # tags=['task'],
     summary='Alters task',
     description='Alters a task identified by its UUID',
 )
 async def alter_task(uuid_: uuid.UUID, item: Task, db: DBSession = Depends(get_db)):
     try:
-        update_data = item.dict(exclude_unset=True)
-        db.tasks[uuid_] = db.tasks[uuid_].copy(update=update_data)
+        db.alter_task(uuid_,item)
     except KeyError as exception:
         raise HTTPException(
             status_code=404,
@@ -129,15 +84,15 @@ async def alter_task(uuid_: uuid.UUID, item: Task, db: DBSession = Depends(get_d
         ) from exception
 
 
-@app.delete(
-    '/task/{uuid_}',
-    tags=['task'],
+@router.delete(
+    '/delete/{uuid_}',
+    # tags=['task'],
     summary='Deletes task',
     description='Deletes a task identified by its UUID',
 )
 async def remove_task(uuid_: uuid.UUID, db: DBSession = Depends(get_db)):
     try:
-        del db.tasks[uuid_]
+        db.remove_task(uuid_)
     except KeyError as exception:
         raise HTTPException(
             status_code=404,
